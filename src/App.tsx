2afa47f05717639f34eabab5d35b61c589fdf76c
Tom Colvin <tdcolvin@gmail.com>
2023-03-10 17:26:44 +0000

    Verifies the action code and provides the results

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { FirebaseError } from 'firebase/app';
+import { checkActionCode, getAuth } from 'firebase/auth';
 import { useEffect, useState } from 'react';
+import { AuthProvider, useFirebaseApp } from 'reactfire';
 import './App.scss';
+import useLocationHash from './hooks/useLocationHash';
 import ActionCodeVerificationInfo from './modules/actionCodeVerificationInfo/ActionCodeVerificationInfo';
 import PasswordChange from './modules/passwordChange/PasswordChange';
 
@@ -8,6 +12,10 @@ function App() {
   const [actionCodeVerificationInProgress, setActionCodeVerificationInProgress] = useState(true);
   const [actionCodeVerificationError, setActionCodeVerificationError] = useState<string | undefined>(undefined);
 
+  const actionCode = useLocationHash();
+
+  const auth = getAuth(useFirebaseApp());
+
   useEffect(() => {
     setActionCodeVerificationInProgress(true);
 
@@ -15,26 +23,53 @@ function App() {
       setActionCodeVerificationInProgress(true);
       setActionCodeVerificationError(undefined);
 
-      await new Promise((resolve) => setTimeout(() => resolve(null), 5000));
+      try {
+        await checkActionCode(auth, actionCode);
+      }
+      catch (e) {
+        const errorCode = (e as FirebaseError).code;
+        switch (errorCode) {
+          case "auth/expired-action-code":
+            setActionCodeVerificationError("Your email link has expired. Please try resetting your password again.");
+            break;
+          case "auth/invalid-action-code":
+            setActionCodeVerificationError("Your email link was invalid. Please try resetting your password again.");
+            break;
+          case "auth/user-disabled":
+            setActionCodeVerificationError("Your account has been disabled.");
+            break;
+          case "auth/user-not-found":
+            setActionCodeVerificationError("Your account could not be found.");
+            break;
+          default:
+            setActionCodeVerificationError("Unable to verify your email link. Please confirm you are connected to the internet and try again.");
+            break;
+        }
+        console.error("Error checking action code", e);
+      }
+      finally {
+        setActionCodeVerificationInProgress(false);
+      }
     };
 
     verifyActionCode()
       .catch(e => {
-        setActionCodeVerificationError("Unable to verify your email link. Please confirm you are connected to the internet and try again.");
         console.log("Error verifying action code", e);
       })
       .finally(() => setActionCodeVerificationInProgress(false));
-  }, []);
+  }, [auth, actionCode]);
 
   return (
-    <div className="App">
-      { (actionCodeVerificationInProgress || actionCodeVerificationError) &&
-        <ActionCodeVerificationInfo errorText={ actionCodeVerificationError }/>
-      }
-      { !actionCodeVerificationInProgress && !actionCodeVerificationError && 
-        <PasswordChange />
-      }
-    </div>
+    <AuthProvider sdk={auth}>
+      <div className="App">
+        { (actionCodeVerificationInProgress || actionCodeVerificationError) &&
+          <ActionCodeVerificationInfo errorText={ actionCodeVerificationError }/>
+        }
+        { !actionCodeVerificationInProgress && !actionCodeVerificationError && 
+          <PasswordChange />
+        }
+      </div>
+    </AuthProvider>
   );
 }
 
