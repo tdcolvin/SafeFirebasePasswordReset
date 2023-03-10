72aac277993cd2999c8b95d52e2631035e2ad26a
Tom Colvin <tdcolvin@gmail.com>
2023-03-10 22:01:41 +0000

    Fixed bug where initial state was incorrect

--- src/hooks/useLocationHash.ts
+++ src/hooks/useLocationHash.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 function useLocationHash() {
-  const [locationHash, setLocationHash] = useState("");
+  const [locationHash, setLocationHash] = useState(window.location.hash.substring(1));
   useEffect(() => {
     const handleHashChange = () => setLocationHash(window.location.hash.substring(1));
 
