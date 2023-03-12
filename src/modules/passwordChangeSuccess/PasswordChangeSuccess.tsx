baaa57b0f107c8317599b15767ebb78df11688be
Tom Colvin <tdcolvin@gmail.com>
2023-03-12 23:35:30 +0000

    img alt

--- src/modules/passwordChangeSuccess/PasswordChangeSuccess.tsx
+++ src/modules/passwordChangeSuccess/PasswordChangeSuccess.tsx
@@ -3,7 +3,7 @@ import "./PasswordChangeSuccess.scss"
 
 function PasswordChangeSuccess() {
     return (<>
-        <img src={ ok } />
+        <img src={ ok } alt="" />
         <h1>Your password has been successfully changed</h1>
         <p>You can now close this window and log in as usual with your new password.</p>
     </>);
